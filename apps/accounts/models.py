422feb103ad2cbb17a526cc54162691e4f8e4cdc
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 01:21:23 +0600

    feat: add user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_school = models.BooleanField(default=False, verbose_name='is_school')
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ('username',)
