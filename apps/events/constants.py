bbcb8944c02a88fbe1346be2b16084072d86cdb3
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 02:21:00 +0600

    feat: add contact types

--- apps/events/constants.py
+++ apps/events/constants.py
@@ -5,3 +5,12 @@ organization_types = (
     ('STC', 'sports center'),
     ('COM', 'company'),
 )
+
+contact_types = (
+    ('TEL', 'phone number'),
+    ('INS', 'instagram'),
+    ('FCB', 'facebook'),
+    ('WSP', 'whatsapp'),
+    ('TLG', 'telegram'),
+    ('TWI', 'twitter'),
+)
