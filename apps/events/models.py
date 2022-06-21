8eee9b81fe35e8892dcda7a36b03d56d27b703b4
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 02:27:53 +0600

    feat: add organization and contact models

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,30 @@
+from .constants import organization_types
+from .constants import contact_types
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name='name')
+    type = models.CharField(max_length=3, choices=organization_types, default=organization_types[0], verbose_name='type')
+
+    class Meta:
+        verbose_name = 'organization'
+        verbose_name_plural = 'organizations'
+        ordering = ('type', 'name')
+    
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    type = models.CharField(max_length=3, choices=contact_types, verbose_name='type')
+    value = models.TextField(verbose_name='value')
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, verbose_name='organization')
+
+    class Meta:
+        verbose_name = 'contact'
+        verbose_name_plural = 'contacts'
+        ordering = ('organization', 'type')
+    
+    def __str__(self):
+        return self.value
