74931559e7bb97a4d1404bf6f02b9b7a767f5e57
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 01:30:50 +0600

    feat: add birth date and age counting function

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
+from datetime import date
 
 
 class User(AbstractUser):
     is_school = models.BooleanField(default=False, verbose_name='is_school')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='birth date')
 
     class Meta:
         verbose_name = 'пользователь'
@@ -12,3 +17,8 @@ class User(AbstractUser):
         
     def __str__(self):
         return self.get_full_name()
+    
+    def age(self):
+        today = date.today()
+        age = today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+        return age
