09e02abc88f5a7e25cc5c5b4e71fa69b943305be
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 00:05:31 +0600

    feat: add allowing to access web app from any domain

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Allowing access to the web service from any domain
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
