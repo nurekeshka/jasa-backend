8da4a40be9b5ae915dce3a2c009964777c5dcecd
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 03:22:32 +0600

    feat: add organization model test valid

--- apps/events/tests.py
+++ apps/events/tests.py
@@ -1,3 +1,20 @@
+from apps.events.constants import organization_types
+from apps.events.models import Organization
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+
+
+class OrganizationModelTests(TestCase):
+    valid_organization_name = 'Nazarbayev Intellectual School in Physics Mathematics Direction - Almaty'
+    valid_organization_description = 'Accelerated socio-economic development of Kazakhstan in the early 21st century has caused an urgent need for professionals with a high level of technical, managerial and leadership competencies, so in 2008 year at the initiative of the First President of the Republic of Kazakhstan, Leader of the Nation Nursultan Nazarbayev, a project of creation the Intellectual schools had been launched.'
+
+
+    def test_organization_type_validation_valid(self):
+        try:
+            Organization.objects.create(name=self.valid_organization_name, type=organization_types[1], description=self.valid_organization_description)
+            success = True
+        except ValidationError:
+            success = False
+        
+        self.assertTrue(success)
