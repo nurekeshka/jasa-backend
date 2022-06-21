5bbeae5d68daeb497aca29448e7d15535ac47413
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-22 00:04:48 +0600

    feat: add rest framework, accounts

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.telegram.apps.TelegramConfig',
+    'apps.accounts.apps.AccountsConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
