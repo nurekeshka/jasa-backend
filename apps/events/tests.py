b4371ee979a034947c626b98140a34c3902d05be
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-23 05:27:07 +0600

    feat: add testing invalid information type in organization model

--- apps/events/tests.py
+++ apps/events/tests.py
@@ -9,6 +9,7 @@ class OrganizationModelTests(TestCase):
     valid_organization_name = 'Nazarbayev Intellectual School in Physics Mathematics Direction - Almaty'
     valid_organization_description = 'Accelerated socio-economic development of Kazakhstan in the early 21st century has caused an urgent need for professionals with a high level of technical, managerial and leadership competencies, so in 2008 year at the initiative of the First President of the Republic of Kazakhstan, Leader of the Nation Nursultan Nazarbayev, a project of creation the Intellectual schools had been launched.'
 
+    invalid_organization_type = 'Hello, world!'
 
     def test_organization_type_validation_valid(self):
         try:
@@ -18,3 +19,13 @@ class OrganizationModelTests(TestCase):
             success = False
         
         self.assertTrue(success)
+
+
+    def test_organization_type_validation_invalid(self):
+        try:
+            Organization.objects.create(name=self.valid_organization_name, type=self.invalid_organization_type, description=self.valid_organization_description)
+            success = True
+        except ValidationError:
+            success = False
+        
+        self.assertFalse(success)
