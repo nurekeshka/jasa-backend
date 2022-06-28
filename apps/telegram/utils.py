5ea5476fd71a865c7729c816753e93bd1e95843a
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 12:38:09 +0600

    feat: add telegram user decorator

--- apps/telegram/utils.py
+++ apps/telegram/utils.py
@@ -12,3 +12,18 @@ def notificate_all_users_about_event(event: Event, bot: TeleBot) -> bool:
             photo=event.photo,
             caption=event_text_description.format(event.name, event.date)
         )
+
+
+def telegram_user(function):
+    def inner(message: types.Message):
+        user, created = Telegram.objects.get_or_create(
+            id=message.from_user.id,
+            defaults={
+                'username': message.from_user.username,
+                'first_name': message.from_user.first_name,
+                'last_name': message.from_user.last_name
+            }
+        )
+
+        function(message, user)
+    return inner
