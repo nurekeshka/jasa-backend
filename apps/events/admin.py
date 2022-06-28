283937f1c92a0c4ede6ddfb673a11fb4435c7535
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 11:58:02 +0600

    fix: ModelAdmin instead of Admin

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -3,24 +3,24 @@ from .models import *
 
 
 @admin.register(Organization)
-class OrganizationAdmin(admin.Admin):
+class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'type')
     fields = ('name', 'description', 'type')
 
 
 @admin.register(Contact)
-class ContactAdmin(admin.Admin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = ('type', 'value', 'organization')
     fields = ('type', 'value', 'organization')
 
 
 @admin.register(Tag)
-class TagAdmin(admin.Admin):
+class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     fields = ('name',)
 
 
 @admin.register(Event)
-class EventAdmin(admin.Admin):
+class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'organization', 'date', 'start', 'end')
     fields = ('name', 'organization', 'date', 'start', 'end', 'tags', 'longitude', 'latitude')
