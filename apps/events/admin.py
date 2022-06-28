bc7c73c39dd7e5660e9a48c4c21c6da7e5a740e6
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 18:12:35 +0600

    feat: add notificate all users about event

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,3 +1,4 @@
+from apps.telegram.management.commands import telegram
 from django.contrib import admin
 from .models import *
 
@@ -20,7 +21,14 @@ class TagAdmin(admin.ModelAdmin):
     fields = ('name',)
 
 
+@admin.action(description='Notificate all users about event/s')
+def notificate_all_users_about_event(modeladmin, request, queryset):
+    for event in queryset:
+        telegram.notificate_all_users_about_event(event)
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'organization', 'date', 'start', 'end')
     fields = ('name', 'organization', 'date', 'start', 'end', 'photo', 'tags', 'longitude', 'latitude')
+    actions = (notificate_all_users_about_event,)
