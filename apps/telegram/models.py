6483aff8ccf17b1204a3a8763854f6af6bdfe819
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 12:11:37 +0600

    feat: add telegram

--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Telegram(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, null=False, blank=False, verbose_name='id')
+    username = models.CharField(max_length=255, null=True, blank=True, verbose_name='username')
+    first_name = models.CharField(max_length=255, null=True, blank=True, verbose_name='first name')
+    last_name = models.CharField(max_length=255, null=True, blank=True, verbose_name='last name')
+
+    class Meta:
+        verbose_name = 'телеграм'
+        verbose_name_plural = 'телеграм'
+
+    def get_full_name(self) -> str:
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+    
+    def __str__(self) -> str:
+        return self.get_full_name()
