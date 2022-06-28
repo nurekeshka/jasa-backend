a6f6b90d81dbbd75fbb4854410f635ac718a284b
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 12:51:59 +0600

    feat: add telegram

--- apps/telegram/admin.py
+++ apps/telegram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Telegram
 
-# Register your models here.
+
+@admin.register(Telegram)
+class TelegramAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name')
+    fields = ('username', 'first_name', 'last_name')
