21f083b8ee5ebacdc6d6d22e46cba9119d40e1b1
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 20:09:59 +0600

    feat: add emoji

---
+++ apps/telegram/constants.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class Emoji (Enum):
+    football = '⚽'
+    heart = '❤️'
+    explosion = '💥'
+    bomb = '💣'
+    rocket = '🚀'
+    fire = '🔥'
+    commet = '☄️'
+    lightning = '⚡'
+    danger = '⚠️'
+    stop = '🚫'
+    calendar = '🗓'
+    marker = '📍'
+    clock = '🕖'
+    people = '👥'
+    money = '💰'
+    pencil = '✏️'
+    credit_card = '💳'
+    orange = '🟧'
+    blue = '🟦'
+    green = '🟩'
+    running_guy = '🏃‍♂️'
+    voice = '📢'
