8f7768caecd7368ef7e852f673df56de3c9b22ba
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-28 22:28:18 +0600

    feat: add event message func

--- apps/telegram/interface.py
+++ apps/telegram/interface.py
@@ -1,3 +1,6 @@
+from apps.events.models import Event
+from .constants import MONTHES
+from .constants import Emoji
 
 
 class Start(object):
@@ -6,4 +9,16 @@ class Start(object):
 
 
 class Events(object):
-    message = '{0}\n{1}'
+    def message(event: Event):
+        text = f'{Emoji.exclamation_mark_double.value} {event.name} {Emoji.exclamation_mark_double.value}\n\n'
+        
+        text += f'{Emoji.marker.value} Адрес: {event.address}\n'
+        text += f'{Emoji.calendar.value} Начало:\t{event.start.date().day} {MONTHES[event.start.date().month]} в {event.start.time().strftime("%H:%M")}\n'
+        text += f'{Emoji.clock.value} Конец:\t{event.end.date().day} {MONTHES[event.end.date().month]} в {event.end.time().strftime("%H:%M")}\n\n'
+        
+        text += f'{Emoji.paper_with_pencil.value} {event.description}\n\n'
+
+        for tag in event.tags.all():
+            text += f'#{tag} '
+
+        return text
