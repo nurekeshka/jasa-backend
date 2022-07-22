d31a1e3c43839abebd0aeead5341d8ab2ecabff8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-07-23 00:33:02 +0600

    feat: search message and markup

--- apps/telegram/interface.py
+++ apps/telegram/interface.py
@@ -1,4 +1,4 @@
-from apps.events.models import Event, Organization
+from apps.events.models import Event, Organization, Tag
 from .constants import MONTHES
 from .constants import Emoji
 from telebot import types
@@ -7,6 +7,7 @@ from telebot import types
 class Start(object):
     name = 'start'
     message = 'Добро пожаловать!'
+    photo = 'https://avatars.githubusercontent.com/u/16373529?v=4'
 
 
 class Organizations(object):
@@ -32,18 +33,18 @@ class Events(object):
     callback_data = 'event:{}'
 
     def message(event: Event) -> str:
-        text = f'{Emoji.exclamation_mark_double.value} {event.name} {Emoji.exclamation_mark_double.value}\n\n'
-
-        text += f'{Emoji.marker.value} Адрес: {event.address}\n'
-        text += f'{Emoji.calendar.value} Начало:\t{event.start.date().day} {MONTHES[event.start.date().month]} в {event.start.time().strftime("%H:%M")}\n'
-        text += f'{Emoji.clock.value} Конец:\t{event.end.date().day} {MONTHES[event.end.date().month]} в {event.end.time().strftime("%H:%M")}\n\n'
-        
-        text += f'{Emoji.paper_with_pencil.value} {event.description}\n\n'
+        text = [
+                f'{Emoji.exclamation_mark_double.value} {event.name} {Emoji.exclamation_mark_double.value}\n\n',
+                f'{Emoji.marker.value} Адрес: {event.address}\n',
+                f'{Emoji.calendar.value} Начало:\t{event.start.date().day} {MONTHES[event.start.date().month]} в {event.start.time().strftime("%H:%M")}\n',
+                f'{Emoji.clock.value} Конец:\t{event.end.date().day} {MONTHES[event.end.date().month]} в {event.end.time().strftime("%H:%M")}\n\n',
+                f'{Emoji.paper_with_pencil.value} {event.description}\n\n'
+            ]
 
         for tag in event.tags.all():
-            text += f'#{tag} '
+            text.append(f'#{tag} ')
 
-        return text
+        return ''.join(text)
     
     def markup(event: Event) -> types.InlineKeyboardMarkup:
         inline = types.InlineKeyboardMarkup()
@@ -67,3 +68,33 @@ class Events(object):
 
 class Back(object):
     text = '« Вернуться назад'
+
+
+class Search(object):
+    def message():
+        text = [
+            'Отправьте мне сообщение с одним из предложенных тэгов:\n',
+        ]
+
+        tags = [f'#{x.name}' for x in Tag.objects.all()]
+
+        text.extend(tags)
+        return ' '.join(text)
+
+    def markup(tag: Tag):
+        events = tag.event_set.all()
+
+        if events is None:
+            return
+
+        inline = types.InlineKeyboardMarkup()
+
+        for event in events:
+            inline.add(
+                types.InlineKeyboardButton(
+                    text=event.name,
+                    callback_data=Events.callback_data.format(event.id)
+                )
+            )
+        
+        return inline
